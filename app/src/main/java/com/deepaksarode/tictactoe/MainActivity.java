d407a3baa22062d6fb03aa99f4c4fc6dc2995d72
Deepak Sarode <dsarode013@gmail.com>
2021-01-23 18:42:10 +0530

    First Commit

---
+++ app/src/main/java/com/deepaksarode/tictactoe/MainActivity.java
@@ -0,0 +1,105 @@
+package com.deepaksarode.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Player Representation
+    // 0 - X
+    // 1 - O
+
+    int activePlayer = 0;
+    int[] gameState = { 2 , 2 , 2 , 2 , 2 , 2 , 2 , 2 , 2 };
+
+    //Game State Meanings
+    // 0 - X
+    // 1 - O
+    // 2 - NULL
+
+    int[][]  winPositions = { {0,1,2} , {3,4,5} , {6,7,8} , {0,3,6} , {1,4,7} , {2,5,8} , {0,4,8} , {2,4,6} };
+
+    boolean gameActive = true;
+
+
+    public void playerTap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImg = Integer.parseInt(img.getTag().toString());  // int tap = (int) imgview.getTag();
+
+        TextView reset = findViewById(R.id.resetTextview);
+        reset.setText("");
+
+        if(!gameActive){
+            gameReset(view);
+        }
+
+            if(gameState[tappedImg] == 2){
+            gameState[tappedImg] = activePlayer;
+            img.setTranslationY(-1000f);
+            if(activePlayer == 0){
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.statusTextview);
+                status.setText("O's-Turn Tap to Play");
+            }
+            else{
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.statusTextview);
+                status.setText("X's-Turn Tap to Play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+
+        // Check if any player has Won
+
+        for(int[] winPosition : winPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]]!=2){
+
+                // Any Player has been Won ---- Find which one
+
+                if(gameState[winPosition[0]] == 0) {
+                    TextView winStatus = findViewById(R.id.statusTextview);
+                    winStatus.setText("X has been Won!!!!");
+                }
+                else{
+                    TextView winStatus = findViewById(R.id.statusTextview);
+                    winStatus.setText("O has been Won!!!");
+                }
+                gameActive = false;
+                reset.setText("Touch Anywhere to Play");
+            }
+        }
+    }
+
+    public void gameReset(View view){
+
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++){
+
+            gameState[i] = 2;
+        }
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
